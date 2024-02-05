344f52adfbff6a87e3bc8dfd5d4a26e9c3e1d40a
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-02-04 21:28:26 -0300

    Create Templated Controls

---
+++ App1/GenericRatingControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace App1
+{
+  public sealed class GenericRatingControl : Control
+  {
+    public GenericRatingControl()
+    {
+      this.DefaultStyleKey = typeof(GenericRatingControl);
+    }
+
+
+
+    public int Rating
+    {
+      get { return (int)GetValue(RatingProperty); }
+      set { SetValue(RatingProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Rating.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty RatingProperty =
+        DependencyProperty.Register("Rating", typeof(int), typeof(GenericRatingControl), new PropertyMetadata(1));
+
+
+  }
+}
